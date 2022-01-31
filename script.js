7569e7644a61828c3f3fe0ff6faf75c1334ae210
DJAruun <caargajac@gmail.com>
2022-01-31 21:22:01 +0100

    1

--- script.js
+++ script.js
@@ -50,3 +50,17 @@ function openPage(pageName,elmnt,color) {
 document.getElementById("defaultOpen").click();
 
 
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
